c72172155af8831655cc1fc27275013668d20a3c
OtabekRustamov <otabekrustamov3991@gmail.com>
2026-09-16 14:16:21 +0900

    gsfs

--- app/main.py
+++ app/main.py
@@ -9,6 +9,18 @@ def print_hi(name,age):
     print(f'Hi, my name is {name} {age} old')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_hi(name,age):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, my name is {name} {age} old')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    def print_hi(name, age):
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Hi, my name is {name} {age} old')  # Press Ctrl+F8 to toggle the breakpoint.
+
+        def print_hi(name, age):
+            # Use a breakpoint in the code line below to debug your script.
+            print(f'Hi, my name is {name} {age} old')  # Press Ctrl+F8 to toggle the breakpoint.
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
